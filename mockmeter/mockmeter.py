80c49f9dc46fd5dfc94a84e14501b246abf2e6d5
Overboard <amwroute-git@yahoo.com>
2018-12-07 11:13:40 -0500

    Reduce to single app implementation

--- mockmeter/mockmeter.py
+++ mockmeter/mockmeter.py
@@ -1,140 +1,62 @@
 from pathlib import Path
-from urllib.parse import urljoin, urlencode
+from urllib.parse import urlunsplit
 
 import cherrypy
-# from cherrypy._cpdispatch import Dispatcher
 import requests
+from slugify import slugify
 
-""" Crude attempt at capturing missed URLs using a Tool hook.  This might be workable,
-but it seems like a lot of manual intervention for what might be more straightforward.
-
-Better solution might exist with a custom Dispatch(), or
-Cascading WSGI middleware apps.
-
-Handling the need to serve static files from more than one local dir to /
-by pipelining a second CherryPy app
+""" Serve the meter's static files from a directory, and handle CGI requests via
+exposed handlers.
 """
 
-@cherrypy.tools.register('before_finalize', priority=90)
-def logit():
-    if cherrypy.response.status == 404:
-        print('Whoa -->', cherrypy.response.status, cherrypy.request.path_info)
-        # if 'network.cgi' in cherrypy.request.path_info:
-        #     cherrypy.response.status = None
-        #     cherrypy.response.body = bytes('\n'.join([
-        #         "0",
-        #         "hostname",
-        #         "192.168.0.172",
-        #         "192.168.0.1",
-        #         "255.255.255.0",
-        #         "0",
-        #     ]),'utf-8')
-        #     del cherrypy.response.headers['content-length']
-
-
-def PipelineFactory(pipeline_app):
-
-    class _PipelineFactoryClass(cherrypy.Application):
-        def __init__(self, nextapp):
-            self.nextapp = nextapp
-            super().__init__(pipeline_app())
-
-        def __call__(self, environ, start_response):
-            # relies on nextapp.default() raising InternalRedirect
-            print('Entered pipeline for {}'.format(pipeline_app.__name__))
-            try:
-                me = self.nextapp(environ, start_response)
-                return me
-            except:
-                # try:
-                me = super().__call__(environ, start_response)
-                print(pipeline_app.__name__, 'attempting to handle', cherrypy.request.path_info)
-                return me
-                # except:
-                #     print('darned if i know')
-                #     return
-
-
-    return _PipelineFactoryClass
-
-
-# @cherrypy.tools.logit()
-@cherrypy.tools.staticdir(dir='./cgi', root=Path.cwd()/'resources/mx50')
-@cherrypy.tools.response_headers(headers=[('Content-Type','text/plain')])
-@cherrypy.tools.allow(methods=['GET','POST'], debug=True)
-# appears to serve from statics before methods, change priority?
-class CGIApp(object):
-    def __init__(self):
-        self.base = 'http://10.161.129.197'
-
-    @cherrypy.expose
-    @cherrypy.tools.response_headers(on=False)
-    def appid(self):
-        return """<html>
-          <body>
-          Hello from CGI App
-          </body>
-        </html>"""
-    
-    @cherrypy.expose
-    def example_cgi(self):
-        return bytes('\n'.join([
-            "0",
-            "hostname",
-            "192.168.0.172",
-            "192.168.0.1",
-            "255.255.255.0",
-            "0",
-        ]),'utf-8')
-
-    @cherrypy.expose
-    def protocol_cgi(self, **kwargs):
-        print('here')
-
-    @cherrypy.expose
-    def default(self, attr, *args, **kwargs):
-        # wont trap if url and method param signatures dont match
-        # e.g. default(self, attr) wont trap input.cgi?ms=1
-        # and  default(self, attr, ms) won't trap input.cgi
-        # print(self,'raising internal redirect for',attr)
-        # raise cherrypy.InternalRedirect('')
-        print(self,'default() entered with', 
-            cherrypy.request.method, attr, kwargs)
-
-        print(self,'will serve from live meter for', attr)
-        r = requests.get(urljoin(self.base,attr), params=kwargs)
-        target = self._cp_config['tools.staticdir.root']/self._cp_config['tools.staticdir.dir']/attr
-
-        try:
-            r.raise_for_status()
-        except requests.exceptions.HTTPError:
-            raise cherrypy.HTTPError(r.status_code)
-        else:
-            # with open(target, 'w+b') as fp:
-            #     fp.write(r.content)
-            #     fp.flush()
-            return r.content
-
-# class LiveMeterApp(object):
-#     base = 'http://10.161.129.197'
-#     @cherrypy.expose
-#     def meter(self):
-#         return ('Forwarding app using ', self.base)
 
 @cherrypy.tools.staticdir(dir='./web_pages', root=Path.cwd()/'resources/mx50', index='index.html')
-@cherrypy.tools.allow(methods=['GET','POST'], debug=True)
 class StaticsApp(object):
-    def __init__(self):
-    #     self.appid = self._appid()
-    #     self.default = self._default()
-        self.base = 'http://10.161.129.197'
-        self.cgi = CGIApp()
+    """ Serve the meter's static files from a directory, and handle CGI requests via
+    exposed handlers.
+
+    `cgi_path`  location of captured CGI responses as a pathlib.Path object
+
+    """
+    def __init__(self, cgi_path: Path):
+        self.ip = '10.161.129.197'
+        self.cgi_path = cgi_path
+
+    def _fetch_cgi_resource(self, params: dict, append_params: bool = True):
+        """ Return resource from disk if it exists, otherwise forward the request 
+        to a physical meter.  Responses to forwarded requests are then captured
+        as a local file for future use.  File names are resolved with the request
+        path_info and optionally request parameters.
+
+        `params`    a dict of request parameters
+        `append_params` bool to optionally use parameters in resolving file name
+        """
+        params_for_fn = params if append_params else ''
+        fn = self.cgi_path/slugify(cherrypy.request.path_info+str(params_for_fn))
+        if fn.exists():
+            return fn.open(mode='rb')
+        else:
+            print(self,'will serve from live meter for', fn)
+            source = urlunsplit((cherrypy.request.scheme, self.ip,
+                cherrypy.request.path_info, cherrypy.request.query_string,''))
+            r = requests.request(cherrypy.request.method, source, data=params)
+            try:
+                r.raise_for_status()
+            except requests.exceptions.HTTPError:
+                raise cherrypy.HTTPError(r.status_code)
+            else:
+                with fn.open(mode='wb') as fp:
+                    fp.write(r.content)
+                    fp.flush()
+                return r.content
+            raise cherrypy.HTTPError(500)
 
     @cherrypy.expose
     def appid(self, **kwargs):
         # wont trap if url and method param signatures dont match
         # e.g. appid(self, attr) wont trap input.cgi?ms=1
         # and  appid(self, attr, ms) won't trap input.cgi
+        # TODO: add more information, e.g. folders, version, etc.
         return """<html><body>
         Hello from Statics App via {} with {}
         </body></html>""".format(cherrypy.request.method, kwargs)
@@ -144,43 +66,44 @@ class StaticsApp(object):
         raise cherrypy.HTTPError(500)
 
     @cherrypy.expose
+    @cherrypy.tools.response_headers(headers=[('Content-Type','application/octet-stream')])
+    @cherrypy.tools.allow(methods=['GET'])
+    def m650_cfg(self, *args, **kwargs):
+        """ Handle m650.cfg with a Content-Type header """
+        return self._fetch_cgi_resource(kwargs)
+
+    @cherrypy.expose(['modbus.cgi', 'dnp.cgi'])
+    @cherrypy.tools.response_headers(headers=[('Content-Type','text/plain')])
+    @cherrypy.tools.allow(methods=['GET','POST'])
+    def protocol_cgi(self, ms, *args, **kwargs):
+        """ Handle protocol.cgi, modbus.cgi, dnp.cgi POSTs by including session """
+        return self._fetch_cgi_resource(kwargs)
+
+    @cherrypy.expose
+    @cherrypy.tools.allow(methods=['GET', 'POST'])
     def default(self, attr, *args, **kwargs):
-        print(self,'default() entered with', 
-            cherrypy.request.method, attr, kwargs)
-
-        # redirect to CGI folder
-        print(self,'raising internal redirect for',attr)
-        target_prm = urljoin('cgi/', attr)
-        raise cherrypy.InternalRedirect(target_prm, query_string=urlencode(kwargs))
-        # raise cherrypy.HTTPRedirect(target_prm)
-
-
-# class Capture404Dispatcher(Dispatcher):
-#     def __call__(self, path_info):
-#         x= super().__call__(path_info)
-#         print(path_info,cherrypy.serving.request.handler)
-#         # shows a 404 for any url not exposed in class
-#         # but the url may get served from the statics later
-#         return x
+        """ Default behaviour for emulating CGI requests is to fetch a resource
+        identified without including any parameters for it's name.  Specifically,
+        most GETs need the 'ms' removed from the query string.  Most POST are
+        submits and need the data from the body removed.
+        """
+        return self._fetch_cgi_resource(kwargs, append_params=False)
 
 
 if __name__ == '__main__':
+    cgi_path = Path.cwd()/'resources/mx50/cgi'
+
     conf = {
         '/': {
-            # 'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.sessions.on': True
-            # 'wsgi.pipeline': [
-                # haven't yet been able to pipeline more than 2 apps
-                # perhaps need to somehow de-assert InternalRedirect?
-                # ('meter', PipelineFactory(LiveMeterApp)),
-                # ('cascade', PipelineFactory(CGIApp)),
-            # ]
+            'tools.sessions.on': True,
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Server','Bitronics')]
         },
-        # '/cgi': {
-        #     'tools.staticdir.on': True,
-        #     'tools.staticdir.root': Path.cwd()/'resources/mx50',
-        #     'tools.staticdir.dir': './cgi'
-        # },
+        '/stub.html': {
+            'tools.staticfile.on': True,
+            'tools.staticfile.root': cgi_path,
+            'tools.staticfile.filename': 'stub.html'
+        },
         '/favicon.ico': {
             'tools.staticfile.on': True,
             'tools.staticfile.root': Path.cwd()/'resources/mx50',
@@ -192,10 +115,7 @@ if __name__ == '__main__':
         'server.socket_host': '127.0.0.1',
         'server.socket_port': 4249
     })
-    cherrypy.tree.mount(StaticsApp(), '', conf)
-    # cherrypy.tree.mount(CGIApp(), '/cgi')
-    # cherrypy.tree.mount(LiveMeterApp(), '/live', conf)
-
+    cherrypy.tree.mount(StaticsApp(cgi_path), '', conf)
     cherrypy.engine.start()
     cherrypy.engine.block()
 
